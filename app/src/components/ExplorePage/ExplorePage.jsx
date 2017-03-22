751183997491ad57d6e5c082b199b36719b76ab4
Jahosh <josh91hickman@gmail.com>
2017-03-22 16:17:19 -0700

    add page header

--- app/src/components/ExplorePage/ExplorePage.jsx
+++ app/src/components/ExplorePage/ExplorePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { NavBarContainer } from '../../containers'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
+import { Header, Icon, Image } from 'semantic-ui-react'
+import axios from "axios"
 
 // default view for the app
 // see /src/routes for routes for this.props.children
@@ -9,14 +11,25 @@ class ExplorePage extends React.Component {
   constructor(props) {
     super(props)
     // this.props.checkLogin() // check is Auth0 lock is authenticating after login callback
+    console.log(props)
   }
-
+  grabProfiles() {
+    axios.get('http://localhost:3000/itineraries').then( (resp) => {
+      console.log(resp);
+    });
+  }
   render() {
+    this.grabProfiles();
     return (
       // navbar persists throughout the whole app
       <div>
         {/*<NavBarContainer />*/}
-        <h1> Explore </h1>
+        <Header as='h1' icon textAlign='center'>
+          <Icon name='users' circular />
+          <Header.Content>
+            itineraries
+          </Header.Content>
+        </Header>
       </div>
     )
   }
