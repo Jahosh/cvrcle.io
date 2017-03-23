4413f288abcea1ea0328b715c80439b32e6a33dd
Jahosh <josh91hickman@gmail.com>
2017-03-22 17:01:03 -0700

    add react-router-bootstrap for navlinks, tweak navbar

--- app/src/components/NavBar/NavBar.js
+++ app/src/components/NavBar/NavBar.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarHeader, Nav } from 'react-bootstrap';
-import { Image } from 'semantic-ui-react';
-import { Link } from 'react-router';
+import React, { Component } from 'react'
+import { Navbar, NavbarHeader, Nav, NavItem } from 'react-bootstrap'
+import { Image } from 'semantic-ui-react'
+import { Link } from 'react-router'
+import { LinkContainer } from 'react-router-bootstrap'
 
 // props passed down from redux store
 
 const NavBar = ({ isAuthenticated, onLoginClick, onLogoutClick }) =>
   <div>
-    <Navbar>
+    <Navbar inverse>
       <Navbar.Header>
-        <Link to='/home'>
+          <Link to='/home'>
           <Image className="cvrcle-logo-icon" src='./images/cvrcle-logo-icon.png' />
         </Link>
       </Navbar.Header>
-      <Nav>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+      <Nav pullRight>
+          <LinkContainer to="/explore">
+            <NavItem eventKey={2}>Explore</NavItem>
+          </LinkContainer>
         { !isAuthenticated ? 
-        (<button onClick={onLoginClick} className="navbar-links">Login</button>) : 
-        (<button onClick={onLogoutClick} className="navbar-links">Logout</button> )}
+        (<NavItem eventKey={3} onClick={onLoginClick}>Login</NavItem>) : 
+        (<NavItem eventKey={4} onClick={onLogoutClick}>Logout</NavItem> )}
       </Nav>
+      </Navbar.Collapse>
     </Navbar>
   </div>
 
